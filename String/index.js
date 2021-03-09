0d8bff86d641f1ff72e16d9f9e0fb8e6174d3f36
muhammadshahbaz08 <muhamadshahbaz17@gmail.com>
2021-03-09 12:02:48 +0500

    Added String Source Code

---
+++ String/index.js
@@ -0,0 +1,2 @@
+var str1 = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. \nSit laboriosam laudantium molestias ipsa non in quasi rem, facilis quidem perferendis a optio, at excepturi soluta.\nlabore tenetur provident, pariatur accusantium. "
+console.log(str1);
